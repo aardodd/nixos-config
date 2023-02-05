752cc240891987875e2bf550313837c1db52c4df
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-05 21:27:47 +0000

    Remove home-manager fonts

--- home/aaron/features/desktop/common/default.nix
+++ home/aaron/features/desktop/common/default.nix
@@ -2,16 +2,11 @@
 {
   imports = [
     ./firefox.nix
-    ./font.nix
   ];
 
   xdg.mimeApps.enable = true;
 
   home.packages = with pkgs; [
-    gimp
-    kdenlive
     keepassxc
-    obs-studio
-    thunderbird
   ];
 }

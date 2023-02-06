f80021033d738008a3428c4dc17c9580841fe10f
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-06 16:43:36 +0000

    Add encrypted btrfs

---
+++ hosts/common/hardware/partitions/encrypted-hybrid-btrfs.nix
@@ -0,0 +1,67 @@
+{ disks ? [ "/dev/sda" ], ... }:
+{
+  disk = {
+    main = {
+      type = "disk";
+      device = builtins.elemAt disks 0;
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "boot";
+            type = "partition";
+            start = "0";
+            end = "1M";
+            flags = [ "bios_grub" ];
+          }
+          {
+            name = "ESP";
+            type = "partition";
+            start = "1M";
+            end = "512M";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          }
+          {
+            name = "root";
+            type = "partition";
+            start = "512M";
+            end = "100%";
+            content = {
+              name = "crypted";
+              type = "luks";
+              keyFile = "/tmp/disk.key";
+              content = {
+                type = "btrfs";
+                extraArgs = "-f"; # Override existing partition
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                  };
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                  };
+                  "/nix" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "/persist" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "/log" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/var/log";
+                  };
+                };
+              };
+            };
+          }
+        ];
+      };
+    };
+  };
+}

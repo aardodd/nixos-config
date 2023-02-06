f504b65de97d6a56b474ded4f7e69649cd1f976c
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-06 19:20:04 +0000

    Port btrfs to encrypted variant

--- hosts/common/hardware/partitions/encrypted-hybrid-btrfs.nix
+++ hosts/common/hardware/partitions/encrypted-hybrid-btrfs.nix
@@ -44,8 +44,8 @@
                     mountpoint = "/";
                     postCreateHook = ''
                       mkdir -p $MNTPOINT
-                      mount /dev/mapper/crypted $MNTPOINT -o subvol=/root
-                      btrfs subvolume snapshot -r $MNTPOINT $MNTPOINT/root-blank
+                      mount -t btrfs /dev/disk/by-label/root $MNTPOINT -o subvol=/
+                      btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/root-blank
                       umount $MNTPOINT
                       rm -rf $MNTPOINT
                     '';

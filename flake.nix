6697aaed4ae27806b073baaf799528b5ddff297c
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-16 16:28:32 +0000

    Improve documentation of flake.nix

--- flake.nix
+++ flake.nix
@@ -24,38 +24,48 @@
   description = "My NixOS Configuration";
 
   inputs = {
+    # Nix channel selection.
     # Pick a channel from https://status.nixos.org/
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
-    # Interfacing with Hardware.
-    impermanence.url = "github:nix-community/impermanence";
+    # Interfacing with hardware.
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
+    # Declarative disk management.
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # HomeManager as a dotfiles replacement.
+    # Help for erasing darlings.
+    impermanence.url = "github:nix-community/impermanence";
+
+    # Manage home directory with home-manager.
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Fix command-not-found
+    # Fix command-not-found.
     programsdb = {
       url = "github:wamserma/flake-programs-sqlite";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Software and Tweaks
+    # Firefox addons.
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Secrets management.
-    sops-nix.url = "github:mic92/sops-nix";
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:

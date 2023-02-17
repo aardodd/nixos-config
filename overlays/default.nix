af196938db0dc99db6b9a72d6b2b85124300b960
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-17 19:32:18 +0000

    Setup mutliple nixpkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,6 +3,16 @@
   # This one brings our custom packages from the 'packages' directory
   additions = final: _prev: import ../packages { pkgs = final; };
 
+  # Unstable nixpkgs
+  unstable = final: prev: {
+    unstable = inputs.nixpkgs-unstable.legacyPackages.${final.system};
+  };
+
+  # Master nixpkgs
+  master = final: prev: {
+    master = inputs.nixpkgs-master.legacyPackages.${final.system};
+  };
+
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays

1ffc957f34284c5a252f1b25fe0e280906896d89
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-20 21:01:49 +0000

    Add cli apps

--- home/aaron/features/cli/default.nix
+++ home/aaron/features/cli/default.nix
@@ -1,19 +1,28 @@
 { pkgs, ... }: {
   imports = [
+    ./bat.nix
     ./fish.nix
     ./git.nix
     ./nix-index.nix
+    ./pfetch.nix
+    ./ranger.nix
     ./ssh.nix
     ./starship.nix
   ];
 
   home.packages = with pkgs; [
-    distrobox
-    hledger
-    jq
-    just
-    nixfmt
-    restic
-    ripgrep
+    bc # Calculator
+    bottom # System viewer
+    comma # Install and run programs by sticking a , before them
+    distrobox # Integrate docker images into the environment
+    exa # Better ls
+    fd # Better find
+    httpie # Better curl
+    hledger # Finance tracking
+    jq # JSON pretty printer and manipulator
+    ncdu # TUI disk usage
+    nixfmt # Nix formatter
+    restic # Backups
+    ripgrep # Better grep
   ];
 }

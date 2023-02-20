23f6bd44af78550d360e8cf70aeef8a1c3784f7c
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-20 15:57:15 +0000

    Remove herbstluftwm

--- home/aaron/features/desktop/common/x11-wm/alacritty.nix
+++
@@ -1,5 +0,0 @@
-{ config, inputs, lib, pkgs, ... }: {
-  programs.alacritty = {
-    enable = true;
-  };
-}
--- home/aaron/features/desktop/common/x11-wm/rofi.nix
+++
@@ -1,5 +0,0 @@
-{ config, inputs, lib, pkgs, ... }: {
-  programs.rofi = {
-    enable = true;
-  };
-}

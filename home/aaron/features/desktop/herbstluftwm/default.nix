3ddc90575b5e04d2c4b3ad2d26b1ab3a656df7cb
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-02-20 14:01:52 +0000

    Add herbstluft tags

--- home/aaron/features/desktop/herbstluftwm/default.nix
+++ home/aaron/features/desktop/herbstluftwm/default.nix
@@ -33,6 +33,28 @@
       Mod4-Control-Up = "resize up +0.05";
       Mod4-Control-Down = "resize down +0.05";
 
+      Mod4-1 = "use_index 1";
+      Mod4-2 = "use_index 2";
+      Mod4-3 = "use_index 3";
+      Mod4-4 = "use_index 4";
+      Mod4-5 = "use_index 5";
+      Mod4-6 = "use_index 6";
+      Mod4-7 = "use_index 7";
+      Mod4-8 = "use_index 8";
+      Mod4-9 = "use_index 9";
+      Mod4-0 = "use_index 10";
+
+      Mod4-Shift-1 = "move_index 1";
+      Mod4-Shift-2 = "move_index 2";
+      Mod4-Shift-3 = "move_index 3";
+      Mod4-Shift-4 = "move_index 4";
+      Mod4-Shift-5 = "move_index 5";
+      Mod4-Shift-6 = "move_index 6";
+      Mod4-Shift-7 = "move_index 7";
+      Mod4-Shift-8 = "move_index 8";
+      Mod4-Shift-9 = "move_index 9";
+      Mod4-Shift-0 = "move_index 10";
+
       Mod4-period = "use_index +1 --skip-visible";
       Mod4-comma = "use_index -1 --skip-visible";
 
@@ -46,5 +68,8 @@
       Mod4-Tab = "cycle_all +1";
       Mod4-Shift-Tab = "cycle_all -1";
     };
+    tags = [
+      "1" "2" "3" "4" "5" "6" "7" "8" "9" "0"
+    ];
   };
 }

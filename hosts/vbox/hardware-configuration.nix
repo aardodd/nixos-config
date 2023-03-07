6ccead3f26f0124192035c464779383a4c85de3c
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-03-07 08:51:16 +0000

    Add vmware support to vbox

--- hosts/vbox/hardware-configuration.nix
+++ hosts/vbox/hardware-configuration.nix
@@ -21,4 +21,5 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   virtualisation.virtualbox.guest.enable = true;
+  virtualisation.vmware.guest.enable = true;
 }

e5b079b3f5f8c54effbc51cfed424980097bb5c5
Aaron Dodd <contact@aaronrdodd.com>
2023-03-09 11:39:27 +0000

    Add commit signing by default
    Signed-off-by: Aaron Dodd <contact@aaronrdodd.com>

--- home/aaron/features/cli/git.nix
+++ home/aaron/features/cli/git.nix
@@ -17,11 +17,15 @@ in {
     delta.enable = true;
     extraConfig = {
       commit.template = "${commitTemplate}";
+      commit.gpgSign = true;
       feature.manyFiles = true;
       format.signoff = true;
+      gpg.ssh.allowedSignersFile = "~/.config/git/allowed_signers";
+      gpg.format = "ssh";
       init.defaultBranch = "main";
       pull.rebase = "true";
       push.default = "current";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
     };
     ignores = [ ".direnv" "result" ];
   };

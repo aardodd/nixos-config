269ec7e8b096b70f8ed197740b1a82f531c8a96a
Aaron Dodd <8947937+aardodd@users.noreply.github.com>
2023-03-09 09:56:12 +0000

    Update git settings
    Signed-off-by: Aaron Dodd <8947937+aardodd@users.noreply.github.com>

--- home/aaron/features/cli/git.nix
+++ home/aaron/features/cli/git.nix
@@ -1,12 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  name = "Aaron Dodd";
+  email = "8947937+aardodd@users.noreply.github.com";
+  commitTemplate = pkgs.writeTextFile {
+    name = "aaron-commit-template";
+    text = ''
+      Signed-off-by: ${name} <${email}>
+    '';
+  };
+in {
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
-    userName = "Aaron Dodd";
-    userEmail = "8947937+aardodd@users.noreply.github.com";
+    userName = name;
+    userEmail = email;
+    delta.enable = true;
     extraConfig = {
+      commit.template = "${commitTemplate}";
       feature.manyFiles = true;
+      format.signoff = true;
       init.defaultBranch = "main";
+      pull.rebase = "true";
+      push.default = "current";
     };
     ignores = [ ".direnv" "result" ];
   };

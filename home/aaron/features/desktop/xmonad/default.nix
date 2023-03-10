3b111b80c3abdbeb7c358c1ffb190ca54f066a5f
Aaron Dodd <contact@aaronrdodd.com>
2023-03-10 22:16:54 +0000

    Automatically start selected window-manager
    Signed-off-by: Aaron Dodd <contact@aaronrdodd.com>

--- home/aaron/features/desktop/xmonad/default.nix
+++ home/aaron/features/desktop/xmonad/default.nix
@@ -12,6 +12,26 @@
     };
   };
 
+  programs = {
+    bash.initExtra = ''
+      if [[ -z $DISPLAY && [ "$(tty)" == "/dev/tty1" ]]; then
+        exec startx &> /dev/null
+      fi
+    '';
+
+    fish.loginShellInit = ''
+      if test -z "$DISPLAY" -a (tty) = '/dev/tty1'
+        exec startx &> /dev/null
+      end
+    '';
+
+    zsh.initExtra = ''
+      if [[ -z $DISPLAY && [ "$(tty)" == "/dev/tty1" ]]; then
+        exec startx &> /dev/null
+      fi
+    '';
+  };
+
   home.file = {
     ".xinitrc".text = ''
       exec xmonad
